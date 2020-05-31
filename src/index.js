913cac381c7519059bcfb32cb7e29117af4a2183
Jordan Surgick <jordansurgick@aol.com>
2020-05-31 18:52:28 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-import React from 'react'
-import { createStore } from 'redux'
-import { combineReducers } from 'redux'
-import App from './App'
-import { books, recommendedBooks} from './reducers'
+import { combineReducers } from 'redux';
+import { books, recommendedBooks } from './reducers';
 
-
-
-let store = createStore(booksManger)
+const rootReducer = combineReducers({books, recommendedBooks})
